bdf94fcf8a0b92e2b88bc132817d7bb26f1b9ba5
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2024-03-18 06:13:59 +0600

    create dynamic api route for specific blog

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 3000;
 
@@ -46,6 +46,13 @@ app.get("/blog", async (req, res) => {
     .skip((page - 1) * size)
     .limit(size);
   const blog = await data.toArray();
+  res.send(blog);
+});
+
+app.get("/blog/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const blog = await blogList.findOne(query);
   res.send(blog);
 });
 
